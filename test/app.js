6d74d3da63e6a3a9e1bdee89de81ff25aa19a928
German Gibbs <gibbsgerman@gmail.com>
2025-04-03 18:24:53 -0400

    cocteaus page

---
+++ test/app.js
@@ -0,0 +1,24 @@
+function myAttribute() {
+    document.getElementById('demo').innerHTML = "Goodbye World";
+
+}
+
+let numeros = [1, 2, 3, 4];
+numeros[2] = 25;
+
+// console.log(numeros);
+
+let division;
+division = 0 / 0;
+// console.log(division);
+
+
+const elMar = ["Debado en el mar", "Descansa en el oceano", "Flota en el oceano"];
+console.log(elMar)
+
+let saltoDeLinea = 'Me gusta ver thrilers\nY escuchar rock';
+// console.log(saltoDeLinea);
+
+
+let stringer = "\"Me llamaba German\"";
+// console.log(stringer);
