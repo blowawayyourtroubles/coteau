7a119ce2cc3f6ce6da8d3f947e5a387f893a0247
German Gibbs <gibbsgerman@gmail.com>
2025-04-03 18:22:47 -0400

    cocteaus tea page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
-let nombre = "German";
 
-console.log(nombre);
+let amount = 125;
+let UserOne = 200;
+let UserTwo = 85;
+
+
+if (amount < UserTwo) {
+    console.log("User already pay the Fee.");
+
+} else {
+    console.log("User must pay the Fee.")
+}
